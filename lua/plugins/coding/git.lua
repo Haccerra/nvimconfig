a4059820a9fe1ac6e047687af5b436d9af0d470d
haccerra <haccerra@aol.com>
2026-05-17 18:22:01 +0200

    Adding git signs in the gutter

---
+++ lua/plugins/coding/git.lua
@@ -0,0 +1,19 @@
+--# selene: allow(mixed_table)
+
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			signs = {
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+		},
+	},
+}
+
