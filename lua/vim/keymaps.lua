cdc3ac896249d21079aaa82030904864d7c10747
haccerra <haccerra@aol.com>
2026-05-17 17:24:36 +0200

    Adding vim keybindings

---
+++ lua/vim/keymaps.lua
@@ -0,0 +1,144 @@
+--# selene: allow(undefined_variable)
+
+local map = vim.keymap.set
+
+-- Better escape
+map("i", "jk", "<Esc>", {
+	desc = "Exit insert mode",
+})
+
+-- Clear search highlights
+map("n", "<Esc>", "<cmd>nohlsearch<CR>", {
+	desc = "Clear search highlight",
+})
+
+-- Save / quit
+map("n", "<leader>w", "<cmd>w<CR>", {
+	desc = "Save file",
+})
+
+map("n", "<leader>q", "<cmd>q<CR>", {
+	desc = "Quit window",
+})
+
+map("n", "<leader>Q", "<cmd>qa<CR>", {
+	desc = "Quit all",
+})
+
+-- Window navigation
+map("n", "<C-h>", "<C-w>h", {
+	desc = "Move to left window",
+})
+
+map("n", "<C-j>", "<C-w>j", {
+	desc = "Move to lower window",
+})
+
+map("n", "<C-k>", "<C-w>k", {
+	desc = "Move to upper window",
+})
+
+map("n", "<C-l>", "<C-w>l", {
+	desc = "Move to right window",
+})
+
+-- Resize windows
+map("n", "<C-Up>", "<cmd>resize +2<CR>", {
+	desc = "Increase window height",
+})
+
+map("n", "<C-Down>", "<cmd>resize -2<CR>", {
+	desc = "Decrease window height",
+})
+
+map("n", "<C-Left>", "<cmd>vertical resize -2<CR>", {
+	desc = "Decrease window width",
+})
+
+map("n", "<C-Right>", "<cmd>vertical resize +2<CR>", {
+	desc = "Increase window width",
+})
+
+-- Buffer navigation
+map("n", "<S-l>", "<cmd>bnext<CR>", {
+	desc = "Next buffer",
+})
+
+map("n", "<S-h>", "<cmd>bprevious<CR>", {
+	desc = "Previous buffer",
+})
+
+map("n", "<leader>bd", "<cmd>bdelete<CR>", {
+	desc = "Delete buffer",
+})
+
+-- Move selected lines
+map("v", "J", ":m '>+1<CR>gv=gv", {
+	desc = "Move selection down",
+})
+
+map("v", "K", ":m '<-2<CR>gv=gv", {
+	desc = "Move selection up",
+})
+
+-- Keep cursor centered
+map("n", "<C-d>", "<C-d>zz", {
+	desc = "Half page down",
+})
+
+map("n", "<C-u>", "<C-u>zz", {
+	desc = "Half page up",
+})
+
+map("n", "n", "nzzzv", {
+	desc = "Next search result",
+})
+
+map("n", "N", "Nzzzv", {
+	desc = "Previous search result",
+})
+
+-- Better paste over selection without replacing clipboard
+map("x", "<leader>p", [["_dP]], {
+	desc = "Paste without yanking replaced text",
+})
+
+-- System clipboard
+map({ "n", "v" }, "<leader>y", [["+y]], {
+	desc = "Yank to system clipboard",
+})
+
+map("n", "<leader>Y", [["+Y]], {
+	desc = "Yank line to system clipboard",
+})
+
+map({ "n", "v" }, "<leader>d", [["_d]], {
+	desc = "Delete without yank",
+})
+
+-- Indent while keeping visual selection
+map("v", "<", "<gv", {
+	desc = "Indent left",
+})
+
+map("v", ">", ">gv", {
+	desc = "Indent right",
+})
+
+-- Diagnostics
+map("n", "[d", vim.diagnostic.goto_prev, {
+	desc = "Previous diagnostic",
+})
+
+map("n", "]d", vim.diagnostic.goto_next, {
+	desc = "Next diagnostic",
+})
+
+map("n", "<leader>e", vim.diagnostic.open_float, {
+	desc = "Show line diagnostic",
+})
+
+map("n", "<leader>xq", vim.diagnostic.setqflist, {
+	desc = "Diagnostics quickfix list",
+})
+
--- lua/vim/opts.lua
+++ lua/vim/opts.lua
@@ -2,9 +2,92 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Tab to spaces conversion
+-- General
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.undofile = true
+vim.opt.confirm = true
+
+-- UI
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.termguicolors = true
+vim.opt.showmode = false
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+-- Indentation
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.breakindent = true
+
+-- Completion
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+-- Better diff
+vim.opt.diffopt:append("linematch:60")
+
+-- Update time
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 500
+
+-- Whitespace display
+vim.opt.list = true
+vim.opt.listchars = {
+	tab = "» ",
+	trail = "·",
+	nbsp = "␣",
+}
+
+-- Keep 4-space indentation for these filetypes too
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("my-indent-settings", { clear = true }),
+	pattern = {
+		"lua",
+		"python",
+		"javascript",
+		"typescript",
+		"javascriptreact",
+		"typescriptreact",
+		"c",
+		"cpp",
+		"java",
+		"cs",
+		"go",
+		"rust",
+		"sh",
+		"bash",
+		"html",
+		"css",
+		"scss",
+		"json",
+		"yaml",
+		"toml",
+		"markdown",
+	},
+	callback = function()
+		vim.opt_local.expandtab = true
+		vim.opt_local.tabstop = 4
+		vim.opt_local.softtabstop = 4
+		vim.opt_local.shiftwidth = 4
+	end,
+})
 
